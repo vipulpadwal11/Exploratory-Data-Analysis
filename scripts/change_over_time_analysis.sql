a15e4716d6e6342f4b7d6388d8406d2d5ccb43b3
vipulpadwal11 <141395792+vipulpadwal11@users.noreply.github.com>
2026-07-28 18:40:02 +0530

    Create change_over_time_analysis.sql for sales analysis
    This SQL script analyzes sales performance over time using different date functions to track trends and metrics.

---
+++ scripts/change_over_time_analysis.sql
@@ -0,0 +1,43 @@
+/*
+Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+*/
+
+-- Analyse sales performance over time
+-- Quick Date Functions
+SELECT
+    YEAR(order_date) AS order_year,
+    MONTH(order_date) AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date);
+
+
+-- DATETRUNC()
+SELECT
+    DATETRUNC(month, order_date) AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date);
+
+
+-- FORMAT()
+SELECT
+    FORMAT(order_date, 'yyyy-MMM') AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM');
