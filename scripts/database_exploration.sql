69474f593acddca1a97888fc4012e9ca4163e230
vipulpadwal11 <141395792+vipulpadwal11@users.noreply.github.com>
2026-07-28 18:27:06 +0530

    Update database_exploration.sql

--- scripts/database_exploration.sql
+++ scripts/database_exploration.sql
@@ -2,7 +2,6 @@
 Purpose:
     - To explore the structure of the database, including the list of tables and their schemas.
     - To inspect the columns and metadata for specific tables.
-===============================================================================
 */
 
 -- Retrieve a list of all tables in the database
